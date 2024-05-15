6cdad61e72b1b637a117fe7e46f1b99332ccb32d
Cyraullie <cyril.goldenschue@gmail.com>
2024-05-15 12:22:29 +0200

    add button to start the game

--- PathSpawner.gd
+++ PathSpawner.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 @onready var path = preload("res://Mobs/Stage1.tscn")
-
+var gameStarter = false
 
 func _on_timer_timeout():
-	var tempPath = path.instantiate()
-	add_child(tempPath)
+	if gameStarter:
+		var tempPath = path.instantiate()
+		add_child(tempPath)
+
+#TODO add wave system
+func _on_start_pressed():
+	gameStarter = true
+	get_parent().get_node("Start").hide()
 	pass # Replace with function body.
