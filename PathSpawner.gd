fab762132a7c6dbb59387de56646c0168735df42
Cyraullie <cyril.goldenschue@gmail.com>
2024-05-15 15:09:32 +0200

    handle all ennemy are dead so spawn new wave

--- PathSpawner.gd
+++ PathSpawner.gd
@@ -1,15 +1,27 @@
 extends Node2D
 
 @onready var path = preload("res://Mobs/Stage1.tscn")
-var gameStarter = false
+var ennemyNumber = 10
+var currEnnemyNumber = 0
 
 func _on_timer_timeout():
-	if gameStarter:
-		var tempPath = path.instantiate()
-		add_child(tempPath)
+	if Game.gameStarter:
+		if currEnnemyNumber < ennemyNumber:
+			currEnnemyNumber += 1
+			var tempPath = path.instantiate()
+			add_child(tempPath)
+		
+		var EnnemyAlive = get_tree().get_root().get_node("Main/PathSpawner").get_child_count()-1
+		
+		if EnnemyAlive == 0:
+			currEnnemyNumber = 0
+			ennemyNumber += 10
+			Game.gameStarter = false
+			get_parent().get_node("Start").show()
+		
 
 #TODO add wave system
 func _on_start_pressed():
-	gameStarter = true
+	Game.gameStarter = true
 	get_parent().get_node("Start").hide()
 	pass # Replace with function body.
